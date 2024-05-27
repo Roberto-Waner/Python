6a709fd20d438982328a15ac7e3cee7d0b3fe40f
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2024-05-27 16:30:16 -0400

    entrenamiento

---
+++ Python/part00.py
@@ -0,0 +1,33 @@
+# comentario
+
+"""
+Este es un 
+comentarios
+"""
+
+'''
+Este es un 
+comentarios
+'''
+
+# consultar el tipo de datos
+
+# print(type("Hola mundo")) # str
+# print(type(5)) #int
+# print(type(1.5)) #float
+# print(type(True)) # bool
+# print(type(1 + 2j)) #complex
+
+my_variable = "Roberto"
+my_variable = "Waner"
+
+MY_CONSTANT = "Mi constante" #las constantes se hacen en mayuscula
+
+my_int = 1
+my_float = 1.5
+my_boolean = True
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = 'mi otras cadena de texto'
+
+print("Hola, Python")
---
+++ Python/part01.py
@@ -0,0 +1,14 @@
+#Variables
+
+my_variable = "My Variable"
+my_int = 5
+
+my_int_to_str = str(my_int)
+print(my_int_to_str)
+print(type(my_int_to_str))
+
+my_bool = True
+my_complex = 1 + 3j
+my_float = 2.3
+
+print(my_variable, my_int_to_str, my_bool, my_complex, my_float)
